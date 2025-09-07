1f06581cb33dbf6cb5802a691c4fc9bc1830bffa
Sakshigadade12345 <sakshigadade3@gmail.com>
2025-09-07 20:54:38 +0530

    Delete QuizProgressRepository.java

--- QuizProgressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.examproject;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizProgressRepository extends JpaRepository<QuizProgressEntity, Integer> {
-    QuizProgressEntity findByUserIdAndQuizName(int userId, String quizName);
-}
