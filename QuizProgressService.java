70258f54df8f2e63df0f5a8adef35ffa9c9c781a
Sakshigadade12345 <sakshigadade3@gmail.com>
2025-09-07 20:54:51 +0530

    Delete QuizProgressService.java

--- QuizProgressService.java
+++
@@ -1,6 +0,0 @@
-package com.example.examproject;
-
-public interface QuizProgressService {
-    void saveProgress(QuizProgress progress);
-    QuizProgress loadProgress(int userId, String quizName);
-}
